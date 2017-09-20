41182b7183c6b46bb460ec9b1ed6dd5761f5aa58
Kapica,  Norbert <norbert.kapica@mobica.com>
2017-09-20 11:45:47 +0200

    JS based resolution detection added;

---
+++ assets/js/resolution_info.js
@@ -0,0 +1,10 @@
+var isMobile = function (a) {
+    var height = window.screen.height
+    var width = window.screen.width
+    
+    if (height > width) {
+        return true;
+
+    } else
+        return true;
+}
